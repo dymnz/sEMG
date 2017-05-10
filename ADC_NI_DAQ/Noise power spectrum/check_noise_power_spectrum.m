738dbcc3b19d6e1526f756b1b14cef345512cb6a
vicodin1123 <vicodin1123@gmail.com>
2017-05-10 14:17:11 +0800

    fuck

---
+++ ADC_NI_DAQ/Noise power spectrum/check_noise_power_spectrum.m
@@ -0,0 +1,28 @@
+% Constant
+file_path = '/Users/wangshunxing/Work/PortableSEMG/Signals/';
+filename = '1.lvm';
+fs = 5000; % 5kHz Sampling rate
+NoiseRange = [1:10000];
+Length = length(NoiseRange);
+
+% Read data from .lvm
+datamat = lvmread(strcat(file_path, filename));
+noise = datamat(NoiseRange, 2);
+
+% Plot
+plot(noise);
+
+title(sprintf('sEMG singal: %s', filename), 'FontSize', 20);
+xlabel('samples', 'FontSize', 20); ylabel('voltage (V)', 'FontSize', 20)
+ylim([-10 10])
+
+% Power spectrum
+X = fftshift(fft(noise));
+fshift = (-Length/2:Length/2-1)*(fs/Length); % zero-centered frequency range
+powershift = 20*log10(abs(X));     % zero-centered power
+figure;
+semilogx(fshift, powershift, '-')
+
+title('FFT of Noise', 'FontSize', 20)
+xlabel('Frequency (Hz)', 'FontSize', 20)
+ylabel('Magnitude (dB)', 'FontSize', 20)
