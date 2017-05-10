567a7991ace28b7e93bda92c2079a60f0480e144
vicodin1123 <vicodin1123@gmail.com>
2017-05-10 14:22:18 +0800

    fuck

--- ADC_NI_DAQ/Noise power spectrum/check_noise_power_spectrum.m
+++ ADC_NI_DAQ/Noise power spectrum/check_noise_power_spectrum.m
@@ -10,7 +10,7 @@ datamat = lvmread(strcat(file_path, filename));
 noise = datamat(NoiseRange, 2);
 
 % Plot
-plot(noise);
+%plot(noise);
 
 title(sprintf('sEMG singal: %s', filename), 'FontSize', 20);
 xlabel('samples', 'FontSize', 20); ylabel('voltage (V)', 'FontSize', 20)
