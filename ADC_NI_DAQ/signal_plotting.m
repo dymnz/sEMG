01d0b81fa3196d5d87cdef3e7a9a9d6a2abd916a
vicodin1123 <vicodin1123@gmail.com>
2017-05-10 13:42:20 +0800

    fcu

--- ADC_NI_DAQ/signal_plotting.m
+++ ADC_NI_DAQ/signal_plotting.m
@@ -1,9 +1,14 @@
-
+% Constant
 file_path = '/Users/wangshunxing/Work/PortableSEMG/Signals/';
-
 filename = '3.lvm';
+SamplingRate = 5000; % 5kHz Sampling rate
 
-% [num,txt,raw] = xlsread(strcat(file_path, filename));
+% Read data from .lvm
 datamat = lvmread(strcat(file_path, filename));
 
-plot(datamat(:, 2))
+% Plot
+plot(datamat(:, 1), datamat(:, 2));
+
+title(sprintf('sEMG singal: %s', filename), 'FontSize', 20);
+xlabel('time (sec)', 'FontSize', 20); ylabel('voltage (V)', 'FontSize', 20)
+ylim([-10 10])
