156ce0ff1ec50ad8e5a6b1acea39125ae144becc
dymnz <vicodin1123@gmail.com>
2018-08-10 00:58:09 +0800

    data

--- Signals/Arduino/Format_semg_angle_magn/outlier_stats.m
+++ Signals/Arduino/Format_semg_angle_magn/outlier_stats.m
@@ -1,21 +1,25 @@
-close all;
-figure;
-boxplot(data, 'Labels',{'FLX', 'EXT', 'PRO','SUP'});
-set(findobj(gca, 'type', 'line'), 'linew', 3);
-
-title('RMSDown');
-set(gca,'fontsize', 30);
-xlabel('Gesture'); ylabel('RMSE (degree)');
+mean_list = zeros(10, 4);
+for i = 1 : 10
+s_idx = (i - 1) * 4 + 1;
+data = all_RMS_list(:, s_idx:s_idx+3);
 
 outlier_list = isoutlier(data, 'quartiles');
 outlier_list = ~outlier_list;
 
-mean_list = zeros(1, 4);
-for i = 1 : 4
-    clean_data = data(outlier_list(:, i), i);
-    mean_list(i) = mean(clean_data);
+
+for r = 1 : 4
+    clean_data = data(outlier_list(:, r), r);
+    mean_list(i, r) = mean(clean_data);
 end
 mean_list
 % fprintf('median:\t');
 % fprintf('%.3f\t', median(ica_data));
 % fprintf('\n');
+
+end
+
+
+%%
+hold on;
+boxplot(mean_list, 'Labels',{'FLX', 'EXT', 'PRO','SUP'});
+title('RMSDown v.s. nICA - 10x average');
